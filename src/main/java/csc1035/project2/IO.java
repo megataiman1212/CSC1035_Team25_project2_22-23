23a1b1520649d5dcb2b757bdc86726164212a36b
Michael-Plummer <m.plummer1@newcastle.ac.uk>
2023-03-04 16:15:23 +0000

    Added static method manipulateQuestionData that allows user to select program choice

--- src/main/java/csc1035/project2/IO.java
+++ src/main/java/csc1035/project2/IO.java
@@ -29,6 +29,10 @@ public class IO {
                     System.exit(0);
                 case 2:
                     System.out.println("SAMPLE 2");
+
+                    manipulateQuestionData();
+
+                    System.out.println("HERE 2");
                     System.exit(0);
                 case 3:
                     System.out.println("SAMPLE 3");
@@ -36,6 +40,10 @@ public class IO {
                 case 4:
                     System.out.println("SAMPLE 4");
                     System.exit(0);
+
+                default:
+                    // @TODO add a default case (once we have implemented all over cases)
+                    System.out.println("SAMPLE DEFAULT");
             }
         }
     }
@@ -48,7 +56,7 @@ public class IO {
         System.out.println("========================================");
         System.out.println("Quiz Options");
         System.out.println("----------------");
-        System.out.println("1 - Sample 1");
+        System.out.println("1 - Create, Read, Update or delete a question");
         System.out.println("2 - Sample 2");
         System.out.println("3 - Sample 3");
         System.out.println("4 - Sample 4");
@@ -59,6 +67,49 @@ public class IO {
     // ================== Manipulating Questions =======================
     // =================================================================
 
+
+    /**
+     * Static method that allows the user to select the program option
+     * they would like to complete
+     */
+    public static void manipulateQuestionData(){
+
+        // Brings choice into scope
+        int choice = 0;
+        try{
+            choice = scanner.nextInt();
+        }
+        catch (InputMismatchException exception){
+            System.out.println("Data entered not an int");
+        }
+
+        switch (choice){
+            case 1:
+                System.out.println("Create Question : ");
+
+                createQuestion();
+
+                System.exit(0);
+            case 2:
+                System.out.println("Read Question : ");
+
+                readQuestion();
+
+                System.exit(0);
+            case 3:
+                System.out.println("Edit/Update Question : ");
+
+                updateQuestion();
+
+                System.exit(0);
+            case 4:
+                System.out.println("Delete Question : ");
+
+                deleteQuestion();
+
+                System.exit(0);
+        }
+    }
     public static void createQuestion(){
 
     }
@@ -76,5 +127,7 @@ public class IO {
     }
 
 
+    // =================================================================
+    // =================================================================
     // =================================================================
 }
