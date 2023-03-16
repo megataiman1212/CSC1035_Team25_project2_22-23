a44e1be99b9c100b26c8db08c2cd46cc11afe082
Michael-Plummer <m.plummer1@newcastle.ac.uk>
2023-03-16 22:03:06 +0000

    added Javadoc comment to Topic enum class

--- src/main/java/csc1035/project2/question/Topic.java
+++ src/main/java/csc1035/project2/question/Topic.java
@@ -1,5 +1,13 @@
 package csc1035.project2.question;
 
+/**
+ * Enum class to represent options for question topic
+ * Options are as follows:
+ * - PROGRAMMING
+ * - DATABASES
+ * - ARCHITECTURE
+ * - MATHS
+ */
 public enum Topic {
     PROGRAMMING,
     DATABASES,
